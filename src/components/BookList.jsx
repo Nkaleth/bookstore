b7580e5f3939e0d030f3d5ac8056ac79abf62517
Nkaleth <nseguralu@gmail.com>
2023-03-03 08:27:24 -0500

    Add change to load APIs DATA

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,8 +1,23 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getBookItems } from '../redux/books/bookSlice';
 import Book from './Book';
 
 const BookList = () => {
-  const { bookItems } = useSelector((store) => store.books);
+  const { bookItems, isLoading } = useSelector((store) => store.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBookItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
   return (
     <div className="library">
       {bookItems.map((book) => (
--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,25 +1,16 @@
+/* eslint-disable no-param-reassign */
 import { v4 as uuidv4 } from 'uuid';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/mP2FLQP9kXHxPk2Ztptk/books';
 const initialState = {
-  bookItems: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-    },
-  ],
+  bookItems: [],
+  isLoading: true,
 };
+
+export const getBookItems = createAsyncThunk('books/getBookItems', () => fetch(url)
+  .then((resp) => resp.json())
+  .catch((err) => console.log(err)));
 
 const bookSlice = createSlice({
   name: 'books',
@@ -36,8 +27,20 @@ const bookSlice = createSlice({
     }),
     RemoveBook: (state, { payload }) => ({
       bookItems: state.bookItems.filter((item) => item.item_id !== payload),
-    }
-    ),
+    }),
+    extraReducers: {
+      [getBookItems.pending]: (state) => {
+        state.isLoading = true;
+      },
+      [getBookItems.fulfilled]: (state, action) => {
+        console.log(action);
+        state.isLoading = false;
+        state.bookItems = action.payload;
+      },
+      [getBookItems.rejected]: (state) => {
+        state.isLoading = false;
+      },
+    },
   },
 });
 
